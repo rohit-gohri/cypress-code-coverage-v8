471f78aa40ca6f4fe094182b399ba057406e0f64
Rohit Gohri <code@rohit.page>
2024-07-14 20:50:57 +0200

    debug logs

--- src/lib/register/register-node.js
+++ src/lib/register/register-node.js
@@ -1,3 +1,5 @@
+const debug = require('debug')('code-coverage')
+
 /**
  * @see https://github.com/bcoe/c8/issues/376
  * @see https://github.com/tapjs/processinfo/blob/33c72e547139630cde35a4126bb4575ad7157065/lib/register-coverage.cjs
@@ -9,7 +11,17 @@ export const session = new inspector.Session()
 session.connect()
 session.post('Profiler.enable')
 session.post('Runtime.enable')
-session.post('Profiler.startPreciseCoverage', {
-  callCount: true,
-  detailed: true
-})
+session.post(
+  'Profiler.startPreciseCoverage',
+  {
+    callCount: true,
+    detailed: true
+  },
+  (err) => {
+    if (err) {
+      console.error(err, 'Could not enable coverage in profiler')
+    } else {
+      debug('Enabled coverage in profiler')
+    }
+  }
+)
